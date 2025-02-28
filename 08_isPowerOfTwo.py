1f7bff80c2d8e1fc64c6de4c436d16f9374a730e
Vignesh <vigneshv01@gmail.com>
2025-02-28 08:23:12 -0500

    isPowerOfTwo - Complete

--- 08_isPowerOfTwo.py
+++ 08_isPowerOfTwo.py
@@ -6,44 +6,28 @@
 
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n==0:
+        if n<=0:
             return False
-        elif (n == 1) or (n == 2) or (n == -2):
+        elif n==1:
             return True
-        
-        divisor: int = 2
-        if n<0:
-            divisor = -2
 
+        q: int = int(n/2)
+        r: int = int(n%2)
 
-        q: int = int(n/divisor)
-        r: int = int(n%divisor)
+        isPowerOfTwo: bool = True
+        while q!=1:
+            if r==1:
+                break
 
-        isPowerOfTwo: bool = False
+            r=int(q%2)
+            q = int(q/2)
 
-        if(divisor<0):
-            while r != 1:
-                r = int(q%divisor)
-                q = int(q/divisor)
-                if q == 2:
-                    isPowerOfTwo = False
-                    break
-                elif q == -2:
-                    isPowerOfTwo = True
-                    break
-        else:
-            while r != 1:
-                r = int(q%divisor)
-                q = int(q/divisor)
-                if q == 2:
-                    isPowerOfTwo = True
-                    break
-        
         if r==1:
-            return False
-        else:
-            return isPowerOfTwo
+            isPowerOfTwo = False
+
+        return isPowerOfTwo
     
 
 ob = Solution()
-print(ob.isPowerOfTwo(4))
+
+print(ob.isPowerOfTwo(6))
